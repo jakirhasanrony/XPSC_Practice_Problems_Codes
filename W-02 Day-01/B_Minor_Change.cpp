edb16ac03e92fdf4e380b98ea50dddaf16a13b96
Md. Jakir Hasan Rony <99494672+jakirhasanrony@users.noreply.github.com>
2024-11-03 17:40:33 +0600

    Add files via upload
    added week-02 and day-01 codes

---
+++ W-02 Day-01/B_Minor_Change.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s, t;
+    cin >> s;
+    cin >> t;
+    int count = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+
+        if (s[i] != t[i])
+        {
+            count++;
+        }
+    }
+    cout << count << '\n';
+
+    return 0;
+}
